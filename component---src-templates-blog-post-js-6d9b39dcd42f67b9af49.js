c6793a3e17d6fbffef5f8d10e81dfbf5cd2c358c
Thomas Min <thomas.min.git@gmail.com>
2022-09-03 13:03:46 +0900

    clean up site

--- component---src-templates-blog-post-js-6d9b39dcd42f67b9af49.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[989],{4982:function(e,t,r){r.r(t),r.d(t,{default:function(){return n}});var l=r(7294);function n(e){var t=e.data.markdownRemark;return l.createElement("div",null,l.createElement("h1",null,t.frontmatter.title),l.createElement("small",null,t.frontmatter.date),l.createElement("div",{dangerouslySetInnerHTML:{__html:t.html}}))}}}]);
-//# sourceMappingURL=component---src-templates-blog-post-js-6d9b39dcd42f67b9af49.js.map
